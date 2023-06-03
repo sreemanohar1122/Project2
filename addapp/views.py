b8e6ecf626fa4730fb0c83cd9d5f5f1b0ecd4314
sreemanohar1122 <sreemanohar1122@gmail.com>
2023-06-03 19:50:44 +0530

    Initial commit

---
+++ addapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return render(request,'home.html')
+def add(request):
+    x=request.POST["t1"]
+    y=request.POST["t2"]
+    i=int(x)
+    j=int(y)
+    z=i+j
+    res=HttpResponse("The sum is:"+str(z))
+    return res
+# Create your views here.
